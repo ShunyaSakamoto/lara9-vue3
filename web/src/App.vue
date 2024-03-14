01d1f1b16238b8f282181f1bc390b9a34c56b099
ShunyaSakamoto <syunya27212@gmail.com>
2024-03-15 00:46:14 +0900

    [fix]ループにおけるディレクティブの利用

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,63 +1,124 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
-const number = ref(80);
-const showOrNot = computed((): boolean => {
-  // 戻り値変数を初期値falseで用意
-  let showOrNot = false;
-  // 0〜100の乱数を発生
-  const rand = Math.round(Math.random() * 100);
-  // 乱数が50以上ならば、戻り値をtrueに変更
-  if (rand >= 50) {
-    showOrNot = true;
-  }
+const cocktailListInit1: string[] = ["ホワイトレディ", "ブルーハワイ", "ニューヨーク"];
+const cocktailList1 = ref(cocktailListInit1);
 
-  return showOrNot;
-});
+const cocktailListInit2: { [key: number]: string } = {
+  2345: "ホワイトレディ",
+  4412: "ブルーハワイ",
+  6792: "ニューヨーク",
+};
+const cocktailList2 = ref(cocktailListInit2);
 
-const randomNumber = computed((): number => {
-  return Math.round(Math.random() * 100);
-});
+const cocktailListInit3 = new Map<number, string>();
+cocktailListInit3.set(2345, "ホワイトレディ");
+cocktailListInit3.set(4412, "ブルーハワイ");
+cocktailListInit3.set(6792, "ニューヨーク");
+const cocktailList3 = cocktailListInit3;
+
+const whiteLadyInit: {
+  id: number;
+  name: string;
+  price: number;
+  recipe: string;
+} = {
+  id: 2345,
+  name: "ホワイトレディ",
+  price: 1200,
+  recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml",
+};
+const whiteLady = ref(whiteLadyInit);
+
+const cocktailDataListInit1: Cocktail[] = [
+  { id: 2345, name: "ホワイトレディ", price: 1200 },
+  { id: 4412, name: "ブルーハワイ", price: 1500 },
+  { id: 6792, name: "ニューヨーク", price: 1100 },
+  { id: 8429, name: "マティーニ", price: 1500 },
+];
+const cocktailDataList1 = ref(cocktailDataListInit1);
+interface Cocktail {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const cocktailDataListInit2 = new Map<number, Cocktail>();
+cocktailDataListInit2.set(2345, { id: 2345, name: "ホワイトレディ", price: 1200 });
+cocktailDataListInit2.set(4412, { id: 4412, name: "ブルーハワイ", price: 1500 });
+cocktailDataListInit2.set(6792, { id: 6792, name: "ニューヨーク", price: 1100 });
+cocktailDataListInit2.set(8492, { id: 8492, name: "マティーニ", price: 1500 });
+const cocktailDataList2 = ref(cocktailDataListInit2);
 </script>
 
 <template>
   <div>
-    <p v-if="number >= 50">条件に合致したので表示①</p>
-    <p v-if="Math.round(Math.random() * 100) >= 50">条件に合致したので表示②</p>
-    <p v-if="showOrNot">条件に合致したので表示③</p>
+    <ul>
+      <li v-for="cocktailName in cocktailList1" v-bind:key="cocktailName">{{ cocktailName }}</li>
+    </ul>
+    <ul>
+      <li v-for="(cocktailName, index) in cocktailList1" v-bind:key="cocktailName">
+        {{ cocktailName }}（インデックス{{ index }}）
+      </li>
+    </ul>
   </div>
   <hr />
 
   <div>
-    <p>
-      点数は{{ randomNumber }}点で
-      <span v-if="randomNumber >= 80">優です。</span>
-      <span v-else-if="randomNumber >= 70">良です。</span>
-      <span v-else-if="randomNumber >= 60">可です。</span>
-      <span v-else>不可です。</span>
-    </p>
+    <ul>
+      <li v-for="(cocktailName, id) in cocktailList2" v-bind:key="'cocktailList' + id">
+        IDが{{ id }}のカクテルは{{ cocktailName }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(cocktailName, id, index) in cocktailList2" v-bind:key="'cocktailListWithIdx' + id">
+        {{ index + 1 }}: IDが{{ id }}のカクテルは{{ cocktailName }}
+      </li>
+    </ul>
   </div>
   <hr />
 
   <div>
-    <p>
-      点数は{{ randomNumber }}点
-      <template v-if="randomNumber >= 80">
-        で優です。
-        <span style="color: red">すばらしい!</span>
+    <ul>
+      <li v-for="[id, cocktailName] in cocktailList3" v-bind:key="id">IDが{{ id }}のカクテルは{{ cocktailName }}</li>
+    </ul>
+  </div>
+  <hr />
+
+  <div>
+    <dl>
+      <template v-for="(value, key) in whiteLady" v-bind:key="key">
+        <dt>{{ key }}</dt>
+        <dd>{{ value }}</dd>
       </template>
-    </p>
+    </dl>
   </div>
   <hr />
 
   <div>
-    <section>
-      v-ifを利用
-      <p v-if="showOrNot">条件に合致したので表示</p>
-    </section>
-    <section>
-      v-showを利用
-      <p v-show="showOrNot">条件に合致したので表示</p>
-    </section>
+    <ul>
+      <li v-for="cocktailItem in cocktailDataList1" v-bind:key="cocktailItem.id">
+        {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
+      </li>
+    </ul>
+  </div>
+  <hr />
+
+  <div>
+    <ul>
+      <li v-for="[id, cocktailItem] in cocktailDataList2" v-bind:key="id">
+        {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
+      </li>
+    </ul>
+  </div>
+  <hr />
+
+  <div>
+    <ul>
+      <li v-for="r in 5" v-bind:key="r">半径{{ r }}の円の円周: {{ 2 * r * 3.14 }}</li>
+    </ul>
+    <select>
+      <option v-for="year in 60" v-bind:key="year" v-bind:value="year + 1965">{{ year + 1965 }}</option>
+    </select>
   </div>
 </template>

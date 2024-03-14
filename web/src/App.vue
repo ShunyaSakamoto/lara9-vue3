b991a2d13202fea3fd77aa73693397b74048e0bc
ShunyaSakamoto <syunya27212@gmail.com>
2024-03-14 23:08:27 +0900

    [fix]条件分岐におけるディレクティブの利用

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,44 +1,63 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
-const htmlStr = ref(`<a href="https://vuejs.org//">VueElement.jsのTOPページ</a>`);
+const number = ref(80);
+const showOrNot = computed((): boolean => {
+  // 戻り値変数を初期値falseで用意
+  let showOrNot = false;
+  // 0〜100の乱数を発生
+  const rand = Math.round(Math.random() * 100);
+  // 乱数が50以上ならば、戻り値をtrueに変更
+  if (rand >= 50) {
+    showOrNot = true;
+  }
 
-const price = ref(1000);
+  return showOrNot;
+});
 
-const hello = ref("hello!");
+const randomNumber = computed((): number => {
+  return Math.round(Math.random() * 100);
+});
 </script>
 
 <template>
   <div>
-    <section>{{ htmlStr }}</section>
-    <section v-html="htmlStr"></section>
+    <p v-if="number >= 50">条件に合致したので表示①</p>
+    <p v-if="Math.round(Math.random() * 100) >= 50">条件に合致したので表示②</p>
+    <p v-if="showOrNot">条件に合致したので表示③</p>
   </div>
   <hr />
 
   <div>
-    <section v-pre>
-      <p v-on:click="showHello">{{ hello! }}</p>
-    </section>
+    <p>
+      点数は{{ randomNumber }}点で
+      <span v-if="randomNumber >= 80">優です。</span>
+      <span v-else-if="randomNumber >= 70">良です。</span>
+      <span v-else-if="randomNumber >= 60">可です。</span>
+      <span v-else>不可です。</span>
+    </p>
+  </div>
+  <hr />
+
+  <div>
+    <p>
+      点数は{{ randomNumber }}点
+      <template v-if="randomNumber >= 80">
+        で優です。
+        <span style="color: red">すばらしい!</span>
+      </template>
+    </p>
   </div>
   <hr />
 
   <div>
     <section>
-      <input type="number" v-model="price" />円
-      <br />
-      <p>金額は{{ price }}円です。</p>
-      <p v-once>金額は{{ price }}です。</p>
+      v-ifを利用
+      <p v-if="showOrNot">条件に合致したので表示</p>
+    </section>
+    <section>
+      v-showを利用
+      <p v-show="showOrNot">条件に合致したので表示</p>
     </section>
   </div>
-  <hr />
-
-  <div>
-    <p v-cloak>{{ hello }}</p>
-  </div>
 </template>
-
-<style>
-[v-cloak] {
-  display: none;
-}
-</style>

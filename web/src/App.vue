2839efaa5d3e9026ee944e191d255a52b5bd9166
ShunyaSakamoto <syunya27212@gmail.com>
2024-03-14 19:04:20 +0900

    [fix]双方向データバインディングの利用

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,98 +1,115 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const randValue = ref("まだです");
-const onButtonClick = (): void => {
-  const rand = Math.round(Math.random() * 10);
-  randValue.value = String(rand);
+const inputNameModel = ref("双方向");
+
+const inputNameBind = ref("Sakamoto");
+const inputNameOn = ref("John Doe");
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  inputNameOn.value = element.value;
 };
 
-const mousePointerX = ref(0);
-const mousePointerY = ref(0);
-const onImgMousemove = (event: MouseEvent): void => {
-  mousePointerX.value = event.offsetX;
-  mousePointerY.value = event.offsetY;
+const inputName2Way = ref("双方向");
+const onInputName2Way = (event: Event): void => {
+  const element = event.target as HTMLInputElement;
+  inputName2Way.value = element.value;
 };
 
-const pBgColor = ref("white");
-const onPClick = (bgColor: string): void => {
-  pBgColor.value = bgColor;
-};
+const inputTextarea = ref("テキストエリアへの入力文字。\n改行も加えてみる。");
+const memberType = ref(1);
+const memberTypeSelect = ref(1);
+const isAgreed = ref(false);
+const isAgreed01 = ref(0);
+const selectedOS = ref([]);
+const selectedOSSelect = ref([]);
 
-const pMsg = ref("イベント前(ここをクリック！)");
-const pBgColorEvent = ref("white");
-const onPClickWithEvent = (bgColor: string, event: MouseEvent): void => {
-  pBgColorEvent.value = bgColor;
-  pMsg.value = event.timeStamp.toString();
-};
-
-const sendMsg = ref("未送信");
-const onFormSubmit = (): void => {
-  sendMsg.value = "送信されました。";
-};
-
-const msg = ref("まだです。");
-const onEnterKey = (): void => {
-  msg.value = "エンターキーが押下されました。";
-};
-const onRightButtonClick = (): void => {
-  msg.value = "ボタンが右クリックされました。";
-};
-const onShiftClick = (): void => {
-  msg.value = "シフトを押しながらクリックされました。";
-};
+const trimedText = ref("");
 </script>
 
 <template>
   <div>
-    <h3>イベントリスナを設定するディレクティブ v-on</h3>
     <section>
-      <button v-on:click="onButtonClick">クリック</button>
-      <p>クリック結果: {{ randValue }}</p>
+      <input type="text" v-model="inputNameModel" />
+      <p>{{ inputNameModel }}</p>
     </section>
   </div>
   <hr />
 
   <div>
-    <h3>メソッドを引数としてイベントオブジェクトを受け取る</h3>
     <section>
-      <img src="./assets/logo.png" alt="Vueのロゴ" width="200" v-on:mousemove="onImgMousemove" />
-      <p>ポインタの位置: x={{ mousePointerX }}; y={{ mousePointerY }}</p>
+      <input type="text" :value="inputNameBind" />
+    </section>
+    <br />
+    <section>
+      <input type="text" v-on:input="onInputName" />
+      <p>{{ inputNameOn }}</p>
     </section>
   </div>
   <hr />
 
   <div>
-    <h3>イベントオブジェクト以外を引数とするイベントハンドラメソッド</h3>
-    <p v-on:click="onPClick('red')" v-bind:style="{ backgroundColor: pBgColor }">
-      ここをクリックすると背景色が変わります。
-    </p>
+    <section>
+      <input type="text" :value="inputName2Way" v-on:input="onInputName2Way" />
+      <p>{{ inputName2Way }}</p>
+    </section>
   </div>
   <hr />
 
   <div>
-    <h3>イベントオブジェクトとその他の引数を併用するイベントハンドラメソッド</h3>
-    <p v-on:click="onPClickWithEvent('green', $event)" v-bind:style="{ backgroundColor: pBgColorEvent }">
-      {{ pMsg }}
-    </p>
+    <textarea v-model="inputTextarea"></textarea>
+    <br />
+    <section>
+      <label><input type="radio" name="memberType" value="1" v-model="memberType" />通常会員</label>
+      <label><input type="radio" name="memberType" value="2" v-model="memberType" />特別会員</label>
+      <label><input type="radio" name="memberType" value="3" v-model="memberType" />優良会員</label>
+      <br />
+      <p>選択されたラジオ: {{ memberType }}</p>
+    </section>
+    <br />
+    <section>
+      <select v-model="memberTypeSelect">
+        <option value="1">通常会員</option>
+        <option value="2">特別会員</option>
+        <option value="3">優良会員</option>
+      </select>
+      <br />
+      <p>選択されたリスト: {{ memberTypeSelect }}</p>
+    </section>
+    <br />
+    <section>
+      <label><input type="checkbox" v-model="isAgreed" />同意する</label>
+      <p>同意の結果: {{ isAgreed }}</p>
+    </section>
+    <section>
+      <label><input type="checkbox" v-model="isAgreed01" true-value="1" false-value="0" />同意する</label>
+      <p>同意の結果: {{ isAgreed01 }}</p>
+    </section>
+    <section>
+      <label><input type="checkbox" v-model="selectedOS" value="1" />macOS</label>
+      <label><input type="checkbox" v-model="selectedOS" value="2" />windows</label>
+      <label><input type="checkbox" v-model="selectedOS" value="3" />Linux</label>
+      <label><input type="checkbox" v-model="selectedOS" value="4" />iOS</label>
+      <label><input type="checkbox" v-model="selectedOS" value="5" />Android</label>
+      <p>選択されたOS: {{ selectedOS }}</p>
+    </section>
+    <section>
+      <select v-model="selectedOSSelect" multiple>
+        <option value="1">macOS</option>
+        <option value="2">windows</option>
+        <option value="3">Linux</option>
+        <option value="4">iOS</option>
+        <option value="5">Android</option>
+      </select>
+      <p>選択されたOS: {{ selectedOSSelect }}</p>
+    </section>
   </div>
   <hr />
 
   <div>
-    <h3>v-onの修飾子 prevent修飾子</h3>
-    <form action="#" v-on:submit.prevent="onFormSubmit">
-      <input type="text" required />
-      <button type="submit">送信</button>
-    </form>
-    <p>{{ sendMsg }}</p>
-  </div>
-  <hr />
-
-  <div>
-    <h3>クリックイベントとキーイベントの修飾子</h3>
-    <p>{{ msg }}</p>
-    <input type="text" v-on:keydown.enter="onEnterKey" /><br />
-    <button v-on:click.right="onRightButtonClick">右クリック</button><br />
-    <button v-on:click.shift="onShiftClick">シフトを押しながらクリック</button><br />
+    <section>
+      <input type="text" v-model.trim="trimedText" />
+      <p>入力文字列: {{ trimedText }}</p>
+    </section>
   </div>
 </template>

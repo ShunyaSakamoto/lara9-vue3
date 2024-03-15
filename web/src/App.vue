fee482d72cbcebfc6bc821da1f1b8d41c81b1257
ShunyaSakamoto <syunya27212@gmail.com>
2024-03-15 17:12:28 +0900

    [fix]ウォッチャーの利用

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,168 +1,152 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
-const cocktailDataListInit1: Cocktail[] = [
-  { id: 2345, name: "ホワイトレディ", price: 1200 },
-  { id: 4412, name: "ブルーハワイ", price: 1500 },
-  { id: 6792, name: "ニューヨーク", price: 1100 },
-  { id: 8492, name: "マティーニ", price: 1500 },
-];
-const cocktailDataList1 = ref(cocktailDataListInit1);
-const cocktail1500_1 = computed((): Cocktail[] => {
-  // 配列のfilter()メソッドを使って新たな配列を生成
-  const newList = cocktailDataList1.value.filter(
-    // filter()メソッドの絞り込み条件関数
-    // 引数は配列の各要素であるCocktailオブジェクト
-    (cocktailItem: Cocktail): boolean => {
-      // 値段が1500かどうかの結果を戻り値とする
-      return cocktailItem.price == 1500;
-    }
-  );
-
-  return newList;
-});
 interface Cocktail {
   id: number;
   name: string;
   price: number;
 }
 
-const cocktailListInit: string[] = ["ホワイトレディ", "ブルーハワイ", "ニューヨーク"];
-const cocktailList = ref(cocktailListInit);
-const changeCocktailList = (): void => {
-  cocktailList.value = ["バラライカ", "XYZ", "マンハッタン"];
-};
-const addCocktailList = (): void => {
-  cocktailList.value.push("ブルームーン");
-};
-const deleteFromCocktailList = (): void => {
-  cocktailList.value.pop();
-};
-
-const whiteLadyInit: {
-  id: number;
-  name: string;
-  price: number;
-  recipe: string;
-} = {
-  id: 2345,
-  name: "ホワイトレディ",
-  price: 1200,
-  recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml",
-};
-const whiteLady = ref(whiteLadyInit);
-const changeWhiteLadyPrice = (): void => {
-  whiteLady.value.price = 1500;
-};
-
-const cocktailDataListInit2 = new Map<number, Cocktail>();
-cocktailDataListInit2.set(2345, { id: 2345, name: "ホワイトレディ", price: 1200 });
-cocktailDataListInit2.set(4412, { id: 4412, name: "ブルーハワイ", price: 1500 });
-cocktailDataListInit2.set(6792, { id: 6792, name: "ニューヨーク", price: 1100 });
-cocktailDataListInit2.set(8429, { id: 8429, name: "マティーニ", price: 1500 });
-const cocktailDataList2 = ref(cocktailDataListInit2);
-const cocktail1500_2 = computed(
-  // 算出関数、戻り値の型はMap
-  (): Map<number, Cocktail> => {
-    // 絞り込んだ要素を格納する新しいMapを用意
-    const newList = new Map<number, Cocktail>();
-    // cocktailDataList内のMapをループ処理
-    cocktailDataList2.value.forEach(
-      // ループの各処理内容を表す関数
-      (value: Cocktail, key: number): void => {
-        // カクテルの価格が1500円ならnewListに登録
-        if (value.price >= 1500) {
-          newList.set(key, value);
-        }
-      }
-    );
-
-    // 絞り込んだ新しいMapを戻り値とする
-    return newList;
+// カクテルリストデータを用意
+const cocktailDataListInit = new Map<number, Cocktail>();
+cocktailDataListInit.set(1, { id: 1, name: "ホワイトレディ", price: 1200 });
+cocktailDataListInit.set(2, { id: 2, name: "ブルーハワイ", price: 1200 });
+cocktailDataListInit.set(3, { id: 3, name: "ニューヨーク", price: 1200 });
+cocktailDataListInit.set(4, { id: 4, name: "マティーニ", price: 1200 });
+// カクテル番号のテンプレート変数を用意
+const cocktailNo1 = ref(1);
+// カクテル番号に対応するカクテル情報の算出プロパティを用意
+const priceMsg1 = computed((): string => {
+  // カクテル番号に該当するカクテルデータを取得
+  const cocktail = cocktailDataListInit.get(cocktailNo1.value);
+  // カクテル番号に該当する情報がない場合のメッセージを用意
+  let msg = "該当カクテルはありません。";
+  // カクテル番号に該当する情報があるなら
+  if (cocktail != undefined) {
+    // カクテル番号に該当するカクテルの名前と金額を表示する文字列を生成
+    msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
   }
+
+  // 表示文字列をリターン
+  return msg;
+});
+// cocktailNo1を1秒ごとに1〜4の乱数を使って変更
+setInterval((): void => {
+  cocktailNo1.value = Math.round(Math.random() * 3) + 1;
+}, 1000);
+
+// カクテル番号のテンプレート変数を用意
+const cocktailNo2 = ref(1);
+// カクテル番号に対応するカクテル情報のテンプレート変数を用意
+const priceMsg2 = ref("");
+// watchEffect2を設定
+watchEffect((): void => {
+  priceMsg2.value = getCocktailInfo(cocktailNo2.value);
+});
+// cocktailNoを1秒ごとに1〜4の乱数を使って変更
+setInterval((): void => {
+  cocktailNo2.value = Math.round(Math.random() * 3) + 1;
+}, 1000);
+// カクテル番号に対応するカクテル情報を取得する関数
+function getCocktailInfo(cocktailNo: number): string {
+  const cocktailDataListInit = new Map<number, Cocktail>();
+  cocktailDataListInit.set(1, { id: 1, name: "ホワイトレディ", price: 1200 });
+  cocktailDataListInit.set(2, { id: 2, name: "ブルーハワイ", price: 1200 });
+  cocktailDataListInit.set(3, { id: 3, name: "ニューヨーク", price: 1200 });
+  cocktailDataListInit.set(4, { id: 4, name: "マティーニ", price: 1200 });
+  // カクテル番号に該当するカクテルデータを取得
+  const cocktail = cocktailDataListInit.get(cocktailNo);
+  // カクテル番号に該当する情報がない場合のメッセージを用意
+  let msg = "該当カクテルはありません。";
+  // カクテル番号に該当する情報があるなら
+  if (cocktail != undefined) {
+    // カクテル番号に該当するカクテルの名前と金額を表示する文字列を生成
+    msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`;
+  }
+
+  // 表示文字列をリターン
+  return msg;
+}
+
+// カクテル番号のテンプレート変数を用意
+const cocktailNo3 = ref(1);
+// カクテル番号に対応するカクテル情報のテンプレート変数を用意
+const priceMsg3 = ref("");
+watch(cocktailNo3, (): void => {
+  priceMsg3.value = getCocktailInfo(cocktailNo3.value);
+});
+// cocktailNo3を1秒ごとに1〜4の乱数を使って変更
+setInterval((): void => {
+  cocktailNo3.value = Math.round(Math.random() * 3) + 1;
+}, 1000);
+
+// カクテル番号のテンプレート変数を用意
+const cocktailNo4 = ref(1);
+// カクテル番号に対応するカクテル情報のテンプレート変数を用意
+const priceMsg4 = ref("");
+watch(
+  cocktailNo4,
+  (): void => {
+    priceMsg4.value = getCocktailInfo(cocktailNo4.value);
+  },
+  { immediate: true }
 );
-const changeWhiteLadyPriceInList = (): void => {
-  const whiteLady = cocktailDataList2.value.get(2345) as Cocktail;
-  whiteLady.price = 1500;
-};
+// cocktailNo4を1秒ごとに1〜4の乱数を使って変更
+setInterval((): void => {
+  cocktailNo4.value = Math.round(Math.random() * 3) + 1;
+}, 1000);
+
+// カクテル番号のテンプレート変数を用意
+const cocktailNo5 = ref(1);
+// カクテル番号に対応するカクテル情報のテンプレート変数を用意
+const priceMsg5 = ref("");
+watch(cocktailNo5, (newVal: number, oldVal: number): void => {
+  // 表示用文字列を用意
+  let msg = "前のカクテル: ";
+  msg += getCocktailInfo(oldVal);
+  msg += "現在のカクテル: ";
+  msg += getCocktailInfo(newVal);
+  // 表示用ボジ列をpriceMsg4に設定
+  priceMsg5.value = msg;
+});
+// cocktailNo5を1秒ごとに1〜4の乱数を使って変更
+setInterval((): void => {
+  cocktailNo5.value = Math.round(Math.random() * 3) + 1;
+}, 5000);
 </script>
 
 <template>
   <div>
-    <section>
-      全てのカクテルリスト
-      <ul>
-        <li v-for="cocktailItem in cocktailDataList1" v-bind:key="'cocktailDataList1' + cocktailItem.id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
-    <section>
-      値段が1500円のカクテルリスト
-      <ul>
-        <li v-for="cocktailItem in cocktail1500_1" v-bind:key="'cocktail1500_1' + cocktailItem.id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
+    <h3>算出プロパティの役割</h3>
+    <p>現在のカクテル番号: {{ cocktailNo1 }}</p>
+    <p>{{ priceMsg1 }}</p>
   </div>
   <hr />
 
   <div>
-    <ul>
-      <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
-        {{ cocktailName }}（インデックス{{ index }}）
-      </li>
-    </ul>
-    <p>
-      CocktailListを
-      <button v-on:click="changeCocktailList">変更</button>
-    </p>
-    <p>
-      CocktailListの末尾に「ブルームーン」を
-      <button v-on:click="addCocktailList">追加</button>
-    </p>
-    <p>
-      CocktailListから末尾の要素を
-      <button v-on:click="deleteFromCocktailList">削除</button>
-    </p>
+    <h3>リアクティブ変数の変化を監視する watchEffect()</h3>
+    <p>現在のカクテル番号: {{ cocktailNo2 }}</p>
+    <p>{{ priceMsg2 }}</p>
   </div>
   <hr />
 
   <div>
-    <dl>
-      <template v-for="(value, key) in whiteLady" v-bind:key="key">
-        <dt>{{ key }}</dt>
-        <dd>{{ value }}</dd>
-      </template>
-    </dl>
-    <p>
-      価格を1500円に
-      <button v-on:click="changeWhiteLadyPrice">変更</button>
-    </p>
+    <h3>監視対象を明示する watch()</h3>
+    <p>現在のカクテル番号: {{ cocktailNo3 }}</p>
+    <p>{{ priceMsg3 }}</p>
   </div>
   <hr />
 
   <div>
-    <section>
-      全てのカクテルリスト
-      <ul>
-        <li v-for="[id, cocktailItem] in cocktailDataList2" v-bind:key="'cocktailDataList2' + id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
-    <section>
-      値段が1500円のカクテルリスト
-      <ul>
-        <li v-for="[id, cocktailItem] in cocktail1500_2" v-bind:key="'cocktail1500_2' + id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
-    <p>
-      CocktailDataList2内のホワイトレディの価格を1500円に
-      <button v-on:click="changeWhiteLadyPriceInList">変更</button>
-    </p>
+    <h3>即時実行の watch()</h3>
+    <p>現在のカクテル番号: {{ cocktailNo4 }}</p>
+    <p>{{ priceMsg4 }}</p>
+  </div>
+  <hr />
+
+  <div>
+    <h3>watch() における変更前後の値の利用</h3>
+    <p>現在のカクテル番号: {{ cocktailNo5 }}</p>
+    <p>{{ priceMsg5 }}</p>
   </div>
 </template>

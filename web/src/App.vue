f6fb3f42fea74ebc9a57fbaff0153b73e38aa5e1
ShunyaSakamoto <syunya27212@gmail.com>
2024-03-16 04:44:37 +0900

    [fix]OptionsAPIにおける基本構文の利用

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,33 @@
 <script lang="ts">
-import { defineComponent, computed, reactive, toRefs } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
-  setup() {
-    // リアクティブなテンプレート変数をまとめて用意
-    const data = reactive({
-      PI: 3.14,
-      radius: Math.round(Math.random() * 10),
-    });
-    // 円の算出プロパティを用意
-    const area = computed((): number => {
-      return data.radius * data.radius * data.PI;
-    });
-    // 半径のテンプレート変数に新しい乱数を1秒ごとに格納
-    setInterval((): void => {
-      data.radius = Math.round(Math.random() * 10);
-    }, 1000);
-
-    // テンプレート変数をリターン
+  data() {
     return {
-      // data,
-      ...toRefs(data),
-      area,
+      height: Math.round(Math.random() * 10),
+      width: Math.round(Math.random() * 10),
     };
+  },
+  computed: {
+    area(): number {
+      return this.height * this.width;
+    },
+  },
+  methods: {
+    change(): void {
+      this.height = Math.round(Math.random() * 10);
+      this.width = Math.round(Math.random() * 10);
+    },
   },
 });
 </script>
 
 <template>
   <div>
-    <h3>setup と reactive と toRefs</h3>
-    <!-- <p>半径{{ data.radius }}の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}</p> -->
-    <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+    <h3>OptionsAPI の基本構文</h3>
+    <p>縦が{{ height }}、横が{{ width }}の長方形の面積は{{ area }}</p>
+    <button v-on:click="change">値を変更</button>
   </div>
   <hr />
 </template>
